cd5aa8d3550a4474836de9cc36f870a13fb2793f
skov82 <skov82@ya.ru>
2022-05-03 17:00:08 +0300

    Updates

---
+++ precache-manifest.e4fd50f1aca86fea03fa8d02711e875a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "920c22acf45f078b984c0767fb656b59",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "d807e258d4e9623c0217",
+    "url": "/ToDoList/static/css/main.e7679336.chunk.css"
+  },
+  {
+    "revision": "1d02cbb86154b0e0cbe6",
+    "url": "/ToDoList/static/js/2.de1cfa78.chunk.js"
+  },
+  {
+    "revision": "b5bd5c214617344ce07b75d22283a182",
+    "url": "/ToDoList/static/js/2.de1cfa78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d807e258d4e9623c0217",
+    "url": "/ToDoList/static/js/main.744aa167.chunk.js"
+  },
+  {
+    "revision": "448f1ac8991118963dc3",
+    "url": "/ToDoList/static/js/runtime-main.97949b04.js"
+  }
+]);
