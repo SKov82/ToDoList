87999c3f69c6beca4f33fca6887fa524aa623de4
skov82 <skov82@ya.ru>
2022-05-08 01:10:44 +0300

    08.2

--- src/state/todolist-reducer.ts
+++ src/state/todolist-reducer.ts
@@ -1,26 +1,32 @@
-import {ToDoListType} from '../App';
+import {FilterType, ToDoListType} from '../App';
 import {v1} from 'uuid';
 
 type ActionType = {
-    type: string
-    [key: string]: any
+    type: 'REMOVE-TODOLIST'
+        | 'ADD-TODOLIST'
+        | 'CHANGE-TITLE'
+        | 'CHANGE-FILTER'
+    id?: string
+    title?: string
+    filter?: FilterType
 }
 
 export const todolistReducer = (state: Array<ToDoListType>, action: ActionType): ToDoListType[] => {
-    let newState = [...state]
-
     switch (action.type) {
         case 'REMOVE-TODOLIST':
-            newState = newState.filter(el => el.id !== action.id)
-            break
+            return state.filter(el => el.id !== action.id)
         case 'ADD-TODOLIST':
-            newState = [
-                {id: v1(), title: action.title, filter: 'all'},
-                ...newState]
-            break
+            return [ {id: v1(), title: action.title || '', filter: 'all'},
+                ...state ]
+        case 'CHANGE-TITLE':
+            let tdl = state.find(el => el.id === action.id)
+            if (tdl) tdl.title = action.title || ''
+            return [ ...state ]
+        case 'CHANGE-FILTER':
+            let tdlist= state.find(el => el.id === action.id)
+            if (tdlist) tdlist.filter = action.filter || 'all'
+            return [ ...state ]
         default:
             throw new Error('Unknown action type')
     }
-
-    return newState
 }
