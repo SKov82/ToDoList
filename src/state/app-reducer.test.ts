120b4c156c28ad1d1df4d66c6e08c253f4cb348c
skov82 <skov82@ya.ru>
2022-07-12 15:21:11 +0300

    refactor appReducer types, add setStatus and setError action creators, add tests for appReducer

---
+++ src/state/app-reducer.test.ts
@@ -0,0 +1,24 @@
+import {AppInitStateType, appReducer, setError, setStatus} from './app-reducer';
+
+let startState: AppInitStateType
+
+beforeEach(() => {
+    startState = {
+        status: 'idle',
+        error: null,
+    }
+})
+
+test('set error', () => {
+    const endState = appReducer(startState, setError('newError'))
+
+    expect(endState.error).toBe('newError')
+    expect(endState.status).toBe('idle')
+})
+
+test('set status', () => {
+    const endState = appReducer(startState, setStatus('failed'))
+
+    expect(endState.status).toBe('failed')
+    expect(endState.error).toBe(null)
+})
--- src/state/app-reducer.ts
+++ src/state/app-reducer.ts
@@ -1,12 +1,13 @@
+type StatusType = 'idle' | 'loading' | 'success' | 'failed'
 export type AppInitStateType = {
-    status: 'idle' | 'loading' | 'success' | 'failed'
+    status: StatusType
     error: string | null
 }
-const appState: AppInitStateType = {
+const appInitState: AppInitStateType = {
     status: 'idle',
     error: 'Ошибка !',
 }
-export const appReducer = (state: AppInitStateType = appState, action: ActionType): AppInitStateType => {
+export const appReducer = (state: AppInitStateType = appInitState, action: ActionType): AppInitStateType => {
     switch (action.type) {
         case 'SET_STATUS':
             return {...state, status: action.status}
@@ -16,4 +17,8 @@ export const appReducer = (state: AppInitStateType = appState, action: ActionTyp
             return state
     }
 }
-type ActionType = any
+type ActionType = ReturnType<typeof setStatus>
+    | ReturnType<typeof setError>
+
+export const setStatus = (status: StatusType) => ({type: 'SET_STATUS', status} as const)
+export const setError = (error: string | null) => ({type: 'SET_ERROR', error} as const)
