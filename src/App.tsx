49322955660609f13388e139582e3bbf96e60d20
skov82 <skov82@ya.ru>
2022-07-18 13:36:40 +0300

    add links, styles, refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
 import {AppBar, Button, IconButton, LinearProgress, Toolbar, Typography} from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
 import Alert from './components/ui/Alert'
 import {TDL} from './components/TDL';
 import {useSelector} from 'react-redux';
 import {AppStateType} from './state/store';
 import {StatusType} from './state/app-reducer';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, NavLink } from 'react-router-dom';
 import { Login } from './components/ui/Login';
 
 function App() {
@@ -18,21 +17,13 @@ function App() {
         <div className="App">
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        ToDoLists
+                        <NavLink  to='/' className='link'>
+                            ToDoLists
+                        </NavLink>
                     </Typography>
 
-                    <Button color="inherit">Login</Button>
+                    <Button href='/login' color="inherit">Login</Button>
                 </Toolbar>
             </AppBar>
             <LinearProgress sx={{ bgcolor: 'orange', opacity: `${style}` }} />
